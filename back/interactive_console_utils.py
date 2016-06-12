5118afcadd2d83139ec00edb5dab3dce16bd20d6
Julcity <julcity77@gmail.com>
2016-06-12 11:55:16 +0200

    display 'name' attribut instead of adress if instance has it

--- back/interactive_console_utils.py
+++ back/interactive_console_utils.py
@@ -13,7 +13,10 @@ def compute_instance_list():
             if instances.__len__() < count:
                 instances.append([])
             instances[instances.__len__() - 1].append(className)
-            instances[instances.__len__() - 1].append(str(hex(id(o))))
+            if hasattr(o, 'name') and o.name is not None:
+                instances[instances.__len__() - 1].append(o.name)
+            else:
+                instances[instances.__len__() - 1].append(str(hex(id(o))))
     for i in range(instances.__len__()):
         for j in range(2):
             print(instances[i][j])
